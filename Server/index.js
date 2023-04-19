ed4903c3a85fc9a47e97489cca98d2d173df1ead
MathibelaD <dineomathibela161@gmail.com>
2023-04-19 13:11:40 +0200

    on error typeDefs file

--- Server/index.js
+++ Server/index.js
@@ -1,6 +1,17 @@
 require("dotenv").config();
 const mongUrl = process.env.MONGODB;
+const express = require("express");
+const typeDefs = require("./graphql/typeDefs")
+const { createServer } = require("http");
+const {makeExecutableSchema } = require("@graphql-tools/schema");
 
 (async function() {
-    console.log("im working");
+    const app = express();
+
+    const httpServer = createServer(app);
+
+    const schema = makeExecutableSchema({
+        typeDefs,
+        resolvers,
+    })
 })();
